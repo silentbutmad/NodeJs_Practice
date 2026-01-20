900741220bc155410a42d73def2556ad93ac9800
silentbutmad <mohitmahajan489@gmail.com>
2026-01-20 08:32:54 +0530

    mongodb

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,11 +1,32 @@
 let express = require('express');
+let fs = require('fs');
 let app =express();
 let port = 3333;
+let morgan = require('morgan');
+
+// log / middleware
+app.use(morgan('common',{stream:fs.createWriteStream('./app.log')}));
+
+// html file path
+app.set('views','./src/views');
+
+// view engine
+app.set('view engine','ejs');
 
 //default
 app.get('/',(req,res) => {
-    res.send('hi from Express');
+    //res.send('hi from Express');
+    //res.render('index');
+    res.render('index',{name:'Mohit'})
 });
+
+app.get('/post',(req,res) =>
+{
+    res.send("hi ");
+    res.end();
+})
+
+
 
 app.listen(port,(err) =>{
     if(err) throw err;
